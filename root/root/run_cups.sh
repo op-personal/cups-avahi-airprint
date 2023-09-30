2d144ca581fa803df2114b220a5c95d6369d46f3
op-personal <73725583+op-personal@users.noreply.github.com>
2023-09-30 06:15:45 -0500

    Update run_cups.sh

--- root/root/run_cups.sh
+++ root/root/run_cups.sh
@@ -2,53 +2,58 @@
 set -e
 set -x
 
-# Is CUPSADMIN set? If not, set to default
-if [ -z "$CUPSADMIN" ]; then
-	CUPSADMIN="cupsadmin"
-fi
+# Set default values for CUPSADMIN and CUPSPASSWORD
+: "${CUPSADMIN:=cupsadmin}"
+: "${CUPSPASSWORD:=$CUPSADMIN}"
 
-# Is CUPSPASSWORD set? If not, set to $CUPSADMIN
-if [ -z "$CUPSPASSWORD" ]; then
-	CUPSPASSWORD=$CUPSADMIN
-fi
-
+# Create user if not exists
 if [ "$(grep -ci "$CUPSADMIN" /etc/shadow)" -eq 0 ]; then
 	adduser -S -G lpadmin --no-create-home "$CUPSADMIN"
 fi
 echo "$CUPSADMIN":"$CUPSPASSWORD" | chpasswd
 
+# Create directories and perform cleanup
 mkdir -p /config/ppd
 mkdir -p /services
 rm -rf /etc/avahi/services/*
 rm -rf /etc/cups/ppd
 ln -s /config/ppd /etc/cups
+
+# Copy service files if they exist
 if [ "$(ls -l /services/*.service 2>/dev/null | wc -l)" -gt 0 ]; then
 	cp -f /services/*.service /etc/avahi/services/
 fi
+
+# Copy or create printers.conf
 if [ "$(ls -l /config/printers.conf 2>/dev/null | wc -l)" -eq 0 ]; then
 	touch /config/printers.conf
 fi
 cp /config/printers.conf /etc/cups/printers.conf
 
+# Copy cupsd.conf if it exists and is non-empty
 if [ "$(ls -l /config/cupsd.conf 2>/dev/null | wc -l)" -ne 0 ]; then
 	cp /config/cupsd.conf /etc/cups/cupsd.conf
 fi
 
+# Function for handling file updates
 printerUpdate() {
 	/usr/bin/inotifywait -m -e close_write,moved_to,create /etc/cups |
 		while read -r directory events filename; do
-			if [ "$filename" = "printers.conf" ]; then
+			case "$filename" in
+			"printers.conf")
 				rm -rf /services/AirPrint-*.service
 				/root/airprint-generate.py -d /services
 				cp /etc/cups/printers.conf /config/printers.conf
 				rsync -avh /services/ /etc/avahi/services/
-			fi
-			if [ "$filename" = "cupsd.conf" ]; then
+				;;
+			"cupsd.conf")
 				cp /etc/cups/cupsd.conf /config/cupsd.conf
-			fi
+				;;
+			esac
 		done
 }
 
+# Start avahi daemon and printerUpdate function
 /usr/sbin/avahi-daemon --daemonize --no-drop-root
 printerUpdate &
 exec /usr/sbin/cupsd -f
